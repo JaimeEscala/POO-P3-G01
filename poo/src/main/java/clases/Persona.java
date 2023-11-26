f57afe1ab8fa2633124a61abc12bee68a112f48b
JaimeEscala <jaescala@espol.edu.ec>
2023-11-25 21:50:12 -0500

    Agregue el constructor en Persona

--- poo/src/main/java/clases/Persona.java
+++ poo/src/main/java/clases/Persona.java
@@ -13,6 +13,36 @@ public class Persona {
     private String nombre; 
     private String email;
    
+    public Persona(String cedula, String nombre, String email){
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     
     
 }
