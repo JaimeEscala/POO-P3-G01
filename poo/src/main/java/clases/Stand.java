38122d2b6d78223f0dd0d3e990bc17d546617735
GaboReye <151981810+GaboReye@users.noreply.github.com>
2023-12-03 11:32:28 -0500

    Update Stand.java
    Clases Stand

--- poo/src/main/java/clases/Stand.java
+++ poo/src/main/java/clases/Stand.java
@@ -1,42 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package clases;
 
-/**
- *
- * @author USUARIO
- */
+import java.time.LocalDate;
+
 public class Stand {
-    
-    private String codStand;
-    private String fechaAsignacion;
-    private Persona persona;
-    
-    public Stand(String codStand, String fechaAsignacion, Persona persona){
-        this.codStand = codStand;
+    private static int num_codigo=1;
+    private int codigo;
+    private LocalDate fechaAsignacion;
+    private Persona persona_responsable;
+    public Stand (){
+        this.codigo=(num_codigo++);
+        this.persona_responsable=null;
+        
+    }
+
+    public static int getNum_stand() {
+        return num_codigo;
+    }
+
+    public static void setNum_stand(int num_stand) {
+        Stand.num_codigo = num_stand;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getFechaAsignacion() {
+        return fechaAsignacion;
+    }
+
+    public void setFechaAsignacion(LocalDate fechaAsignacion) {
         this.fechaAsignacion = fechaAsignacion;
-        this.persona = persona;
+    }
+
+    public Persona getPersona_responsable() {
+        return persona_responsable;
+    }
+
+    public void setPersona_responsable(Persona persona_responsable) {
+        this.persona_responsable = persona_responsable;
     }
     
-    public String getCodStand(){
-        return this.codStand;
+    public String toString(){
+        return "Stand{codigo: "+this.codigo+" fecha_asignacion: "+this.fechaAsignacion+" infopersona_responsable: "+this.persona_responsable.toString();
     }
+
     
-    public String getFechaAsignacion(){
-        return this.fechaAsignacion;
-    }
     
-    public Persona getPersona(){
-        return this.persona;
-    }
-    
-    public void setFechaAsignacion(String fechaAsignacion){
-        this.fechaAsignacion = fechaAsignacion;
-    }
-    
-    public void setPersona(Persona persona){
-        this.persona = persona;
-    }
 }
