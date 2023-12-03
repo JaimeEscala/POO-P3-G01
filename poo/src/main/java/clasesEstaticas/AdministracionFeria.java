e2d35b05f460252451b3a1aafda3dbd3f5eb013f
NewtonToledo <152027741+NewtonToledo@users.noreply.github.com>
2023-12-03 11:37:34 -0500

    Update AdministracionFeria.java
    Clase administracion feria

--- poo/src/main/java/clasesEstaticas/AdministracionFeria.java
+++ poo/src/main/java/clasesEstaticas/AdministracionFeria.java
@@ -1,13 +1,175 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package clasesEstaticas;
 
-/**
- *
- * @author jaesc
- */
-public class AdministracionFeria {
+import clases.Emprendedor;
+import clases.Feria;
+import clases.Seccion;
+import clases.Stand;
+import clases.Persona;
+import com.mycompany.proyectoferia.ProyectoFeria;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AdministracionFeria{
+      
+    //ArrayList<Socials> redesSocial = new ArrayList<>(); 
+    
+    public static void info_ferias(){
+        for (Feria f: ProyectoFeria.ferias){
+            System.out.println(f.toString());
+        }
+    }
+    public static void ver_info_feria(int codigo){
+        for (Feria f: ProyectoFeria.ferias){
+            if(f.getCodigo()==codigo){
+                System.out.println(f.toString()+" lista auspiciantes: "+ f.getlAuspiciantes()+" fecha fin: "+f.getfFin()+"horario: "+f.getHorario()+"descripcion: "+f.getDescripcion());
+                int cont=0;
+                for (Seccion s:f.getSecciones()){
+                    cont++;
+                    int numStands=s.getNum_stands();
+                    System.out.println(" Stands en seccion "+cont+": "+numStands );
+                }
+            }
+        }
+    }
+    
+    public static void registrarFeria(String nombre, String lugar, String descripcion, LocalDate fInicio, LocalDate fFin, String horario, int sec1, int sec2, int sec3, int sec4){
+        
+        Feria new_feria=new Feria(nombre, fInicio, fFin, lugar, horario, descripcion);
+        new_feria.getSecciones()[0]=new Seccion(sec1);
+        new_feria.getSecciones()[1]=new Seccion(sec2);
+        new_feria.getSecciones()[2]=new Seccion(sec3);
+        new_feria.getSecciones()[3]=new Seccion(sec4); 
+        ProyectoFeria.ferias.add(new_feria);
+    }
+    
+    public static void modificarFeria(Feria f){
+        System.out.println("Que campo desea modificar:");
+                System.out.println("""
+                           1) Nombre
+                           2) Descripcion
+                           3) Lugar
+                           4) Fecha de Inicio
+                           5) Fecha de Fin
+                           6) Horario
+                           7) Salir
+                           """);
+        boolean salir = false;
+        int opcion;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingresa una opcion:");
+        opcion=sc.nextInt();
+        
+        Scanner sc1 = new Scanner(System.in);
+        while(!salir){
+                
+                
+            switch (opcion) {
+                
+                case 1:
+                    System.out.println("Ingresa el valor de nombre:");
+                    String nombre=sc1.nextLine();
+                    f.setNombre(nombre);
+                    salir = true;
+                    break;
+                case 2:
+                    System.out.println("Ingresa el valor de descripcion:");
+                    String descripcion=sc1.nextLine();
+                    f.setDescripcion(descripcion);
+                    salir = true;
+                    break;
+                case 3:
+                    System.out.println("Ingresa el valor de lugar:");
+                    String lugar=sc1.nextLine();
+                    f.setLugar(lugar);
+                    salir = true;
+                    break;
+                case 4:
+                    System.out.println("Ingresa el valor de la fecha de inicio aa/mm/dd :");
+                    LocalDate ld_current=f.getfInicio();
+                    String fInicio=sc1.nextLine();
+                    String[] partesFecha=fInicio.split("-");
+                    Integer aa=Integer.parseInt(partesFecha[0]);
+                    Integer mm=Integer.parseInt(partesFecha[1]);
+                    Integer dd=Integer.parseInt(partesFecha[2]);
+                    LocalDate fNueva=LocalDate.of(aa,mm, dd);
+                    f.setfInicio(fNueva);
+                    salir = true;
+                    break;
+                case 5:
+                    System.out.println("Ingresa el valor de la fecha de inicio aa/mm/dd :");
+                    LocalDate ld_currentFin=f.getfFin();
+                    String fFin=sc1.nextLine();
+                    String[] partesFechaFin=fFin.split("-");
+                    Integer aaF=Integer.parseInt(partesFechaFin[0]);
+                    Integer mmF=Integer.parseInt(partesFechaFin[1]);
+                    Integer ddF=Integer.parseInt(partesFechaFin[2]);
+                    LocalDate fNuevaF=LocalDate.of(aaF,mmF, ddF);
+                    f.setfFin(fNuevaF);
+                    salir = true;
+                    break;
+                case 6:
+                    System.out.println("Ingresa el valor de horario:");
+                    String horario=sc1.nextLine();
+                    f.setHorario(horario);
+                    salir = true;
+                    break;
+                case 7:
+
+                    System.out.println("Saliendo del menu...");
+                    salir = true;
+
+                    break;
+                default:
+                    System.out.println("Opcion ingresada no valida");
+                    break;
+              
+            }
+        }
+
+    }
+    public static void info_emprendedores(Feria f){
+        int cont=0;
+        for (Seccion s: f.getSecciones()){
+         cont++;
+            
+            for (Stand st: s.getStands()){
+                if (st.getPersona_responsable()!=null){
+                    System.out.println("Seccion" +"["+cont+"]"+ "stand "+"["+st.getCodigo()+"] :");
+                    Emprendedor e=(Emprendedor)st.getPersona_responsable();
+                    String nombre_emprendimiento=e.getNombre();
+                    String descripcion=e.getDescripcion_servicio();
+                    System.out.println("Nombre de emprendimiento:"+nombre_emprendimiento);
+                    System.out.println("Descripcion de emprendimiento: "+descripcion);
+                    
+                } 
+               
+            }
+        }
+    }
+    
+    //OPCIONES 2
+    
+    public static void info_Empre(){
+        for (Emprendedor empre: ProyectoFeria.emprendedores){
+            System.out.println(empre.toString());
+        }
+    }
+    
+    
+    public static void registrarEmprendedor(String identificacion, String nombre,String telefono, String email, String direccion, String sitio_web, String nombre_responsable, ArrayList redes){
+        if(direccion == null && sitio_web == null){
+            Emprendedor new_Emprendedor = new Emprendedor(identificacion, nombre, telefono, email, "", "", nombre_responsable, redes);
+        } else if(direccion == null){
+            Emprendedor new_Emprendedor = new Emprendedor(identificacion, nombre, telefono, email, null, sitio_web, nombre_responsable, redes);
+        } else if(sitio_web == null){
+            Emprendedor new_Emprendedor = new Emprendedor(identificacion, nombre, telefono, email, direccion, null, nombre_responsable, redes);
+        }  
+    }
+    
+    public static void modificar_emprendedor(Emprendedor e){
+    
+    }
     
 }
